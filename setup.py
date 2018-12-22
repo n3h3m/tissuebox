71f411b71760c80b91c851c7ca89e5399761ab23
Nehemiah Jacob <nehemiah.jacob@gmail.com>
2018-12-22 18:29:18 +1100

    Uploaded to PyPi with .rst

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
+with open("pypi.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='tissuebox',
-    version='18.12.7',
+    version='18.12.12',
     description='Tissuebox :: Pythonic payload validator',
     author='nehem',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     author_email='nehemiah.jacob@gmail.com',
     url='https://github.com/nehemiahjacob/tissuebox.git',
     packages=['tissuebox']
